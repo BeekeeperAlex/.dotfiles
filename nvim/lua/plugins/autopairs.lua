476b9f7a570c2409c7878e83f8192114411f184f
Chev <Chev@ChevCast.tv>
2025-02-11 13:19:06 -0700

    chore: plugin updates

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,13 +1,13 @@
 return {
-	-- First disable built-in mini.pairs.
-	{
-		"echasnovski/mini.pairs",
-		enabled = false,
-	},
-	-- Enable nvim-autopairs instead.
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {},
-	},
+	-- 	-- First disable built-in mini.pairs.
+	-- 	{
+	-- 		"echasnovski/mini.pairs",
+	-- 		enabled = false,
+	-- 	},
+	-- 	-- Enable nvim-autopairs instead.
+	-- 	{
+	-- 		"windwp/nvim-autopairs",
+	-- 		event = "InsertEnter",
+	-- 		opts = {},
+	-- 	},
 }

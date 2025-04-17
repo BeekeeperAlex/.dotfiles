6620c357d9542980bdc90cae60b52c9a040dcee8
Chev <Chev@ChevCast.tv>
2025-04-17 04:25:11 -0600

    temp: saving neovide settings in case they fix it soon

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,22 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+if vim.g.neovide then
+	-- vim.o.font = "BigBlueTermPlus Nerd Font"
+	-- vim.g.neovide_floating_corner_radius = 1.0
+	-- vim.g.neovide_opacity = 0.8
+	-- vim.g.neovide_normal_opacity = 0.8
+	-- vim.g.neovide_hide_mouse_when_typing = true
+	-- vim.g.neovide_refresh_rate = 120
+	-- vim.g.neovide_remember_window_size = true
+	-- vim.g.neovide_cursor_antialiasing = true
+	-- vim.g.neovide_cursor_animate_in_insert_mode = true
+	-- vim.g.neovide_cursor_animate_command_line = true
+	--
+	-- vim.g.neovide_cursor_vfx_mode = "railgun"
+	-- vim.g.neovide_cursor_vfx_mode = {"", ""}
+end
+
 vim.g.root_spec = { ".git", "compose.yml", "cwd" }
 vim.g.lazyvim_prettier_needs_config = true
 vim.g.rust_recommended_style = "0"
